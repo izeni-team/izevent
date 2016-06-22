eb87ffd6e8fe2a82a741bb65c1218c26fe6916eb
Bryan Henderson <bhenderson@izeni.com>
2016-06-22 17:14:41 -0600

    Added missing files for Carthage.

---
+++ IZEvent/IZEvent.h
@@ -0,0 +1,19 @@
+//
+//  IZEvent.h
+//  IZEvent
+//
+//  Created by Christopher Bryan Henderson on 6/22/16.
+//  Copyright © 2016 Bryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IZEvent.
+FOUNDATION_EXPORT double IZEventVersionNumber;
+
+//! Project version string for IZEvent.
+FOUNDATION_EXPORT const unsigned char IZEventVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IZEvent/PublicHeader.h>
+
+
